f62001fb2ec1d41a204fa232c0fd3fef17824e73
MichaelTMatthews <michael.tryfan.matthews@gmail.com>
2025-01-13 13:46:26 +0000

    car moves like a spaceship

--- car_physics/car.py
+++ car_physics/car.py
@@ -20,7 +20,7 @@ from jax2d.scene import (
     add_thruster_to_scene,
     add_polygon_to_scene,
 )
-from jax2d.sim_state import StaticSimParams, SimParams
+from jax2d.sim_state import StaticSimParams, SimParams, SimState
 
 
 def make_render_pixels(static_sim_params, screen_dim):
@@ -64,6 +64,8 @@ def make_render_pixels(static_sim_params, screen_dim):
 
         rect_colours = jnp.zeros((static_sim_params.num_polygons, 3))
         rect_colours = rect_colours.at[4, 0].set(255.0)
+        rect_colours = rect_colours.at[5, :].set(255.0)
+        rect_colours = rect_colours.at[8, :].set(255.0)
 
         rect_uniforms = (
             rectangle_vertices_pixel_space,
@@ -138,54 +140,42 @@ def main():
         sim_state, static_sim_params, position=car_pos + car_half_dims * jnp.array([-1.0, 1.0]), dimensions=wheel_dims
     )
 
-    # Create circles for the wheels of the car
-    # sim_state, (_, c1_index) = add_circle_to_scene(
-    #     sim_state, static_sim_params, position=jnp.array([1.5, 1.0]), radius=0.35
-    # )
-    # sim_state, (_, c2_index) = add_circle_to_scene(
-    #     sim_state, static_sim_params, position=jnp.array([2.5, 1.0]), radius=0.35
-    # )
-    #
     # # Join the wheels to the car body with revolute joints
     # # Relative positions are from the centre of masses of each object
-    sim_state, _ = add_revolute_joint_to_scene(
+    sim_state, _ = add_fixed_joint_to_scene(
         sim_state,
         static_sim_params,
         a_index=r_index,
         b_index=w0_index,
         a_relative_pos=car_half_dims * jnp.array([1.0, 1.0]),
         b_relative_pos=jnp.zeros(2),
-        motor_on=False,
     )
 
-    sim_state, _ = add_revolute_joint_to_scene(
+    sim_state, _ = add_fixed_joint_to_scene(
         sim_state,
         static_sim_params,
         a_index=r_index,
         b_index=w1_index,
         a_relative_pos=car_half_dims * jnp.array([1.0, -1.0]),
         b_relative_pos=jnp.zeros(2),
-        motor_on=False,
     )
 
-    sim_state, _ = add_revolute_joint_to_scene(
+    sim_state, _ = add_fixed_joint_to_scene(
         sim_state,
         static_sim_params,
         a_index=r_index,
         b_index=w2_index,
         a_relative_pos=car_half_dims * jnp.array([-1.0, -1.0]),
         b_relative_pos=jnp.zeros(2),
-        motor_on=False,
     )
 
-    sim_state, _ = add_revolute_joint_to_scene(
+    sim_state, _ = add_fixed_joint_to_scene(
         sim_state,
         static_sim_params,
         a_index=r_index,
         b_index=w3_index,
         a_relative_pos=car_half_dims * jnp.array([-1.0, 1.0]),
         b_relative_pos=jnp.zeros(2),
-        motor_on=False,
     )
 
     # Renderer
@@ -200,6 +190,58 @@ def main():
     while True:
         actions = -jnp.ones(static_sim_params.num_joints + static_sim_params.num_thrusters)
         sim_state, _ = step_fn(sim_state, sim_params, actions)
+
+        # Take action
+        wheel_rotate_speed = 0.05
+        if pygame.key.get_pressed()[pygame.K_a]:
+            sim_state = sim_state.replace(
+                polygon=sim_state.polygon.replace(
+                    rotation=sim_state.polygon.rotation.at[w0_index].add(wheel_rotate_speed)
+                )
+            )
+            sim_state = sim_state.replace(
+                polygon=sim_state.polygon.replace(
+                    rotation=sim_state.polygon.rotation.at[w3_index].add(wheel_rotate_speed)
+                )
+            )
+
+        if pygame.key.get_pressed()[pygame.K_d]:
+            sim_state = sim_state.replace(
+                polygon=sim_state.polygon.replace(
+                    rotation=sim_state.polygon.rotation.at[w0_index].add(-wheel_rotate_speed)
+                )
+            )
+            sim_state = sim_state.replace(
+                polygon=sim_state.polygon.replace(
+                    rotation=sim_state.polygon.rotation.at[w3_index].add(-wheel_rotate_speed)
+                )
+            )
+
+        def _apply_wheel_impulse(sim_state, w_index, dir):
+            w_rot = sim_state.polygon.rotation[w_index]
+            w_dv = (
+                jnp.array(
+                    [
+                        -jnp.sin(w_rot),
+                        jnp.cos(w_rot),
+                    ]
+                )
+                * dir
+            )
+
+            sim_state = sim_state.replace(
+                polygon=sim_state.polygon.replace(velocity=sim_state.polygon.velocity.at[w_index].add(w_dv))
+            )
+
+            return sim_state
+
+        if pygame.key.get_pressed()[pygame.K_w]:
+            sim_state = _apply_wheel_impulse(sim_state, w0_index, 1.0)
+            sim_state = _apply_wheel_impulse(sim_state, w3_index, 1.0)
+
+        if pygame.key.get_pressed()[pygame.K_s]:
+            sim_state = _apply_wheel_impulse(sim_state, w0_index, -1.0)
+            sim_state = _apply_wheel_impulse(sim_state, w3_index, -1.0)
 
         # Render
         pixels = renderer(sim_state)
